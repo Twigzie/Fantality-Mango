b34aee28dc449fdfafec24d6785c5ef95bf7d4ea
Twigzie <Paradox_Slayer517@hotmail.com>
2021-03-24 14:27:19 -0500

    Small update to color assets

--- src/scripts/community/assets.js
+++ src/scripts/community/assets.js
@@ -1,23 +1,21 @@
+
 var Assets = {
-    init: () => {
-        try {   
-
-            if (Fantality.utils.params) {
-
-            }
+    init: (source) => {
+        try {
 
             /**
-            * Retrieve the json asset for colors
+            * Retrieve the json asset
             */
-            $.getJSON("https://fantalitystudios.ca/assets/colors/colors.json", (colors) => {
+            $.getJSON(source.endpoint, (cache) => {
 
-                console.log("Current Assets", colors);
-
-                
+                console.log("Assets", cache);
 
                 $(".main-content")
                     .empty()
-                    .append(Handlebars.templates['colors'](colors));
+                    .append(Handlebars.templates[source.template]({
+                        current: cache.current,
+                        selected: source.selected
+                    }));
 
             });
 
@@ -30,10 +28,42 @@ var Assets = {
 
         }
     },
+    GetUrlParams() {
+        var params = {};
+        var search = window.location.search.substring(1);
+        var options = search.split('&');
+        for (var i = 0; i < options.length; i++) {
+            var option = options[i].split('=');
+            if (option && option.length === 2) {
+                if (params.hasOwnProperty(option[0]))
+                    continue;
+                switch (option[0]) {
+                    case "type":     params.type     = option[1]; break;
+                    case "selected": params.selected = option[1]; break;
+                    default: continue;
+                }
+
+            }
+        }
+        return params || {};
+    }
 }
+
+var Endpoints = {
+    colors: {
+        template: "colors",
+        selected: 0,
+        endpoint: "https://fantalitystudios.ca/assets/colors/colors.json"
+    }
+};
 
 $(document).ready(() => {
 
-    Assets.init();
+    var params = Assets.GetUrlParams();
+    if (params && Endpoints[params.type]) {
+
+        Assets.init(Endpoints[params.type]);
+
+    }
 
 });
